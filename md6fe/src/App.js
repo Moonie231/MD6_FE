71d9e49f58b287ef495931dc3b547e076c5818bc
TungND2k2 <satanblueis@gmail.com>
2023-03-11 15:38:57 +0700

    update css

--- md6fe/src/App.js
+++ md6fe/src/App.js
@@ -2,8 +2,6 @@ import './App.css';
 import Home from "./Page/home";
 import Header from "./Component/Header";
 import Footer from "./Component/Footer";
-import LoginMerchant from "./Page/merchant/login";
-import Register from "./Page/merchant/register";
 
 function App() {
   return (
